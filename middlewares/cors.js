e8ff625c0bd1e54988939287b085e2ff27287ec2
Renisme <149890201+Renisme@users.noreply.github.com>
2024-05-22 21:03:22 +0300

    Update cors.js

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -1,7 +1,7 @@
 
   
   
-const allowedCors = ["http://localhost:3000", "http://localhost:3001"];
+const allowedCors = ["https://pindie-zlo.nomoredomainswork.ru", "http://localhost:3000", "http://localhost:3001"];
 
 function cors(req, res, next) {
     const { origin } = req.headers;
